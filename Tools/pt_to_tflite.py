a75eae920c1a4014fde29997e264226c39e6a9dc
Mathias Vansteensel <mathias.vansteensel@yahoo.com>
2025-12-31 01:04:34 +0100

    Add conversion script

--- Tools/pt_to_tflite.py
+++ Tools/pt_to_tflite.py
@@ -2,13 +2,13 @@ from ultralytics import YOLO
 
 # Load the YOLO11 model
 model = YOLO("./runs/detect/train5/weights/best.pt")
-# Export the model to TFLite format
 
-path = model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+# Export the model to TFLite Edge TPU format
+path = model.export(format="edgetpu")  # creates 'yolo11n_full_integer_quant_edgetpu.tflite'
 print("Exported to:", path)
 
-# Load the exported TFLite model
-tflite_model = YOLO("yolo11n_float32.tflite")
+# Load the exported TFLite Edge TPU model
+edgetpu_model = YOLO("yolo11n_full_integer_quant_edgetpu.tflite")
 
 # Run inference
-results = tflite_model("https://ultralytics.com/images/bus.jpg")
+results = edgetpu_model("https://ultralytics.com/images/bus.jpg")
